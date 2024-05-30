a3f8addb0901a748cc4301667c186735467903ba
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-05-30 13:15:57 -0600

    added notes on basic fast api

--- part-01-hello-world/app/main.py
+++ part-01-hello-world/app/main.py
@@ -1,22 +1,23 @@
 from fastapi import FastAPI, APIRouter
 
+# Instantiate the FastAPI object
+app = FastAPI(title="Recipe API", openapi_url="/openapi.json") # this is the main app, the entry we are starting up
 
-app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
-
+# Specify a router for the API
 api_router = APIRouter()
 
-
-@api_router.get("/", status_code=200)
+# Decorator syntax to specify the api route
+@api_router.get("/", status_code=200) # we say the home route "/" will execute the function below
 def root() -> dict:
     """
     Root GET
     """
-    return {"msg": "Hello, World!"}
+    return {"msg": "Hello, World!"} # we return a dictionary which fastAPI converts to json
 
-
+# Include the router in the app
 app.include_router(api_router)
 
-
+# This is for debugging purposes only
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
