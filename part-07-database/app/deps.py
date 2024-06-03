6e6ab84bc448df2b211bf3e44ba75fdf07905d8b
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2024-06-03 12:50:02 -0600

    added notes on dependency inyection

--- part-07-database/app/deps.py
+++ part-07-database/app/deps.py
@@ -7,6 +7,6 @@ def get_db() -> Generator:
     db = SessionLocal()
     db.current_user_id = None
     try:
-        yield db
+        yield db # we yield the db here, so that it is available to use in any of our path operations
     finally:
         db.close()
